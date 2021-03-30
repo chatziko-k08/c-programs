155c017d6bd64808f7381c6086fc4a9d64e91c7f
Kostas Chatzikokolakis <kostas@chatzi.org>
2021-03-30 11:21:31 +0300

    initial version

---
+++ example.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Παράδειγμα προγράμματος που αποτελείται από ένα μόνο αρχείο .c.
+//
+// - Το τρέχουμε με Ctrl-Shift-B (ή Terminal/ Run task / Single file: compile and run)
+// - Κάνουμε debug με F5
+
+int main() {
+    printf("This is a simple C program.\n");
+}
---
+++ multi_file_example/main.c
@@ -0,0 +1,10 @@
+#include "module.h"
+
+// Παράδειγμα προγράμματος που αποτελείται από πολλά αρχεία .c
+//
+// - Το τρέχουμε με Terminal/ Run task / All files in directory: compile and run)
+// - Για debug: View / Run, επιλέγουμε "All files in directory: compile and debug", και F5
+
+int main() {
+    foo();
+}
